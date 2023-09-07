a78086ea3cc878aacf7fb83f5aeba4316ccbeeaf
A7medabdelaty <elqahwagy.123@gmail.com>
2023-09-07 10:51:46 +0300

    add firebase core & refactor routes

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:laza/services/account_info/address/presentation/views/address_view.dart';
+import 'package:laza/services/account_info/payment/presentation/views/new_card_view.dart';
+import 'package:laza/services/account_info/payment/presentation/views/payment_view.dart';
+import 'package:laza/services/auth/forget_password/presentation/views/confirmation_code_view.dart';
+import 'package:laza/services/auth/forget_password/presentation/views/forget_password_view.dart';
+import 'package:laza/services/auth/forget_password/presentation/views/new_password.dart';
+import 'package:laza/services/auth/get_started/presentation/views/get_started_view.dart';
+import 'package:laza/services/auth/login/presentation/views/login_view.dart';
+import 'package:laza/services/auth/register/presentation/views/register_view.dart';
+import 'package:laza/services/cart/presentation/views/cart_view.dart';
+import 'package:laza/services/cart/presentation/views/order_confirmed_view.dart';
+import 'package:laza/services/details/presentation/views/add_review_view.dart';
+import 'package:laza/services/details/presentation/views/product_details_view.dart';
+import 'package:laza/services/details/presentation/views/product_reviews_view.dart';
+import 'package:laza/services/home/presentation/views/all_products_view.dart';
+import 'package:laza/services/home/presentation/views/home_view.dart';
+import 'package:laza/services/on_boarding/presentation/views/on_boarding_view.dart';
+class AppRouter{
+  static Map<String, Widget Function(BuildContext)> appRoutes() {
+    return {
+      OnBoardingView.routeName: (context) => const OnBoardingView(),
+      HomeView.routeName: (context) => const HomeView(),
+      GetStartedView.routeName: (context) => const GetStartedView(),
+      RegisterView.routeName: (context) => const RegisterView(),
+      LoginView.routeName: (context) => const LoginView(),
+      ForgetPasswordView.routeName: (context) => ForgetPasswordView(),
+      ConfirmationCodeView.routeName: (context) => const ConfirmationCodeView(),
+      NewPasswordView.routeName: (context) => const NewPasswordView(),
+      ProductDetailsView.routeName: (context) => const ProductDetailsView(),
+      ProductReviewsView.routeName: (context) => const ProductReviewsView(),
+      AddReviewView.routeName: (context) => const AddReviewView(),
+      CartView.routeName: (context) => const CartView(),
+      AddressView.routeName: (context) => AddressView(),
+      PaymentView.routeName: (context) => const PaymentView(),
+      NewCardView.routeName: (context) => const NewCardView(),
+      OrderConfirmedView.routeName: (context) => const OrderConfirmedView(),
+      NewArrivalView.routeName: (context) => const NewArrivalView(),
+    };
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:laza/core/firebase_service.dart';
+import 'package:laza/core/utils/app_routes.dart';
 import 'package:laza/core/utils/styles/theme.dart';
-import 'package:laza/services/account_info/address/presentation/views/address_view.dart';
-import 'package:laza/services/account_info/payment/presentation/views/new_card_view.dart';
-import 'package:laza/services/account_info/payment/presentation/views/payment_view.dart';
-import 'package:laza/services/auth/forget_password/presentation/views/confirmation_code_view.dart';
-import 'package:laza/services/auth/forget_password/presentation/views/forget_password_view.dart';
-import 'package:laza/services/auth/forget_password/presentation/views/new_password.dart';
-import 'package:laza/services/auth/get_started/presentation/views/get_started_view.dart';
-import 'package:laza/services/auth/login/presentation/views/login_view.dart';
-import 'package:laza/services/auth/register/presentation/views/register_view.dart';
-import 'package:laza/services/cart/presentation/views/cart_view.dart';
-import 'package:laza/services/details/presentation/views/add_review_view.dart';
-import 'package:laza/services/details/presentation/views/product_details_view.dart';
-import 'package:laza/services/home/presentation/views/all_products_view.dart';
-import 'package:laza/services/home/presentation/views/home_view.dart';
 import 'package:laza/services/layout.dart';
-import 'package:laza/services/on_boarding/presentation/views/on_boarding_view.dart';
 
-import 'services/cart/presentation/views/order_confirmed_view.dart';
-import 'services/details/presentation/views/product_reviews_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseService.configureFirebase();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
-        systemStatusBarContrastEnforced: true),
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+      systemStatusBarContrastEnforced: true,
+    ),
   );
   runApp(const MyApp());
 }
@@ -40,26 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppThemes.lightTheme,
-      routes: {
-        OnBoardingView.routeName: (context) => const OnBoardingView(),
-        HomeView.routeName: (context) => const HomeView(),
-        GetStartedView.routeName: (context) => const GetStartedView(),
-        RegisterView.routeName: (context) => const RegisterView(),
-        LoginView.routeName: (context) => const LoginView(),
-        ForgetPasswordView.routeName: (context) => ForgetPasswordView(),
-        ConfirmationCodeView.routeName: (context) =>
-            const ConfirmationCodeView(),
-        NewPasswordView.routeName: (context) => const NewPasswordView(),
-        ProductDetailsView.routeName: (context) => const ProductDetailsView(),
-        ProductReviewsView.routeName: (context) => const ProductReviewsView(),
-        AddReviewView.routeName: (context) => const AddReviewView(),
-        CartView.routeName: (context) => const CartView(),
-        AddressView.routeName: (context) => AddressView(),
-        PaymentView.routeName: (context) => const PaymentView(),
-        NewCardView.routeName: (context) => const NewCardView(),
-        OrderConfirmedView.routeName: (context) => const OrderConfirmedView(),
-        NewArrivalView.routeName: (context) => const NewArrivalView(),
-      },
+      routes: AppRouter.appRoutes(),
       home: const AppLayout(),
     );
   }
