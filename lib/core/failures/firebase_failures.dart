14fe2d7a408af09e5803c32cdbd4fbf12af772bb
A7medabdelaty <elqahwagy.123@gmail.com>
2023-09-08 17:06:04 +0300

    update failure messages

--- lib/core/failures/firebase_failures.dart
+++ lib/core/failures/firebase_failures.dart
@@ -1,10 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:laza/core/failures/failure.dart';
 
-class FirebaseFailure extends Failure{
+class FirebaseFailure extends Failure {
   FirebaseFailure(super.errMessage);
 
-  factory FirebaseFailure.fromAuthException(FirebaseAuthException exception){
-    return FirebaseFailure("${exception.message}");
+  factory FirebaseFailure.fromAuthException(FirebaseAuthException exception) {
+    String message = '';
+    if (exception.code == 'network-request-failed') {
+      message = 'No Internet Connection';
+    } else if (exception.code == "wrong-password") {
+      message = 'Please Enter correct password';
+    } else if (exception.code == 'user-not-found') {
+      message = 'Email not found';
+    } else if (exception.code == 'too-many-requests') {
+      message = 'Too many attempts please try later';
+    } else {
+      message = 'Something went wrong, please try later!!';
+    }
+    return FirebaseFailure(message);
   }
 }
