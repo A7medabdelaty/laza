19fe5ab8b8022fd54be42cd932d04f40671e47b4
A7medabdelaty <elqahwagy.123@gmail.com>
2023-09-16 23:42:14 +0300

    create api service class

---
+++ lib/core/utils/services/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  ApiService(this._dio);
+
+  final String _baseUrl = 'https://api.escuelajs.co/api/v1';
+  final Dio _dio;
+
+  Future<Map<String, dynamic>> get(
+      {required String endPoint, Map<String, dynamic>? queryParams}) async {
+    var response = await _dio.get(
+      _baseUrl + endPoint,
+      queryParameters: queryParams,
+    );
+    return response.data;
+  }
+}
