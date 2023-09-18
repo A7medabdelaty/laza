ca3a96692045c500d02ffef5e2004ff79728e4a7
A7medabdelaty <elqahwagy.123@gmail.com>
2023-09-18 13:45:01 +0300

    add home repo to service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,15 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:laza/core/utils/services/api_service.dart';
 import 'package:laza/core/utils/services/auth_service.dart';
 import 'package:laza/services/auth/forget_password/data/repos/forget_password_repo_impl.dart';
 import 'package:laza/services/auth/get_started/data/repos/get_started_repo_impl.dart';
 import 'package:laza/services/auth/login/data/repos/login_repo_impl.dart';
 import 'package:laza/services/auth/register/data/repos/register_repo_impl.dart';
+import 'package:laza/services/home/data/repos/home_repo_impl.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
   getIt.registerSingleton<AuthService>(
     AuthService(),
+  );
+  getIt.registerSingleton<ApiService>(
+    ApiService(Dio()),
   );
   getIt.registerSingleton<GetStartedRepoImpl>(
     GetStartedRepoImpl(getIt.get<AuthService>()),
@@ -22,5 +28,8 @@ void setupServiceLocator() {
   );
   getIt.registerSingleton<ForgetPasswordRepoImpl>(
     ForgetPasswordRepoImpl(getIt.get<AuthService>()),
+  );
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(getIt.get<ApiService>()),
   );
 }
